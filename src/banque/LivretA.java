1e2df8607427a4e6cead775b29d66c8cf396e0a6
Taya <Taya@10.151.128.127>
2019-07-01 14:53:52 +0200

    ajout de virer(), fix retirer()

--- src/banque/LivretA.java
+++ src/banque/LivretA.java
@@ -2,18 +2,24 @@ package banque;
 
 public class LivretA extends Compte
 {
+	
+	public static void main(String args[])
+	{
+		LivretA la1 = new LivretA();
+		la1.deposer(2000);
+		System.out.println("compte : "+la1);
+		LivretA la2 = new LivretA();
+		la1.virer(1000, la2);
+		System.out.println("compte : "+la2);
+	}
+	
+	
 	@Override
 	public int retirer(int somme)
 	{
-		if(this.getSolde()-somme > 0)
-		{
-			this.solde -= somme;
-		}
-		else
-		{
-			System.out.print("Vous n'avez pas assez d'argent pour faire un retrait");
-		}
-		return getSolde();
+		if(this.getSolde()-somme < 0)
+			throw new IllegalArgumentException("Solde insuffisant");
+		return this.solde -= somme;
 	}
 	
 	@Override
